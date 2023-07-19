fc0ae10708c274e42ecf8ffca0ceddd256e8d228
askjeffit <askjeffit@gmail.com>
2023-07-19 01:23:05 -0600

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 if [ -z $TUNNEL_CLIENT ]; then
   echo "Running wstunnel in server mode"
-  /wstunnel --server -v wss://"${SERVER_ADDRESS}" --restrictTo "$RESTRICT_TO_ADDRESS"
+  /wstunnel -v --server wss://"${SERVER_ADDRESS}" --restrictTo "$RESTRICT_TO_ADDRESS"
 else
   echo "Running wstunnel in client mode"
   /wstunnel -v --udp --udpTimeoutSec="${UDP_TIMEOUT}" -L "${LOCAL_TO_REMOTE}" wss://"${SERVER_ADDRESS}"
